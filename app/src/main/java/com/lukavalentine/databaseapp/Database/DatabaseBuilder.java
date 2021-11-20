b20e4fd355c170ee9486820f02ea8dde778896ab
Pigeon Tastic <13218687+pigeontastic@users.noreply.github.com>
2021-11-20 13:19:37 -0800

    DatabaseBuilder.java completed for now.

--- app/src/main/java/com/lukavalentine/databaseapp/Database/DatabaseBuilder.java
+++ app/src/main/java/com/lukavalentine/databaseapp/Database/DatabaseBuilder.java
@@ -1,4 +1,40 @@
 package com.lukavalentine.databaseapp.Database;
 
-public class DatabaseBuilder {
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Insert;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.lukavalentine.databaseapp.DAO.AssessmentDAO;
+import com.lukavalentine.databaseapp.DAO.CourseDAO;
+import com.lukavalentine.databaseapp.DAO.TermDAO;
+import com.lukavalentine.databaseapp.Entities.AssessmentEntity;
+import com.lukavalentine.databaseapp.Entities.CourseEntity;
+import com.lukavalentine.databaseapp.Entities.TermEntity;
+
+@Database(entities = {AssessmentEntity.class, CourseEntity.class, TermEntity.class}, version = 1, exportSchema = false)
+public abstract class DatabaseBuilder extends RoomDatabase {
+    public abstract AssessmentDAO assessmentDAO();
+    public abstract CourseDAO courseDAO();
+    public abstract TermDAO termDAO();
+
+    private static volatile DatabaseBuilder INSTANCE;
+
+    static DatabaseBuilder getDatabase(final Context context){
+        if (INSTANCE == null){
+            synchronized (DatabaseBuilder.class){
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(), DatabaseBuilder.class, "MyDatabase.db")
+                            .fallbackToDestructiveMigration()
+                            .build();
+                }
+            }
+        }
+
+        return INSTANCE;
+
+    }
+
 }
