24b71b11fa8ad86abcc61bb5586173f3605f97bc
Pigeon Tastic <13218687+pigeontastic@users.noreply.github.com>
2021-11-20 13:26:19 -0800

    Began work on Repository.java.

--- app/src/main/java/com/lukavalentine/databaseapp/Database/Repository.java
+++ app/src/main/java/com/lukavalentine/databaseapp/Database/Repository.java
@@ -1,4 +1,27 @@
 package com.lukavalentine.databaseapp.Database;
 
+import com.lukavalentine.databaseapp.DAO.AssessmentDAO;
+import com.lukavalentine.databaseapp.DAO.CourseDAO;
+import com.lukavalentine.databaseapp.DAO.TermDAO;
+import com.lukavalentine.databaseapp.Entities.AssessmentEntity;
+import com.lukavalentine.databaseapp.Entities.CourseEntity;
+import com.lukavalentine.databaseapp.Entities.TermEntity;
+
+import java.util.List;
+
 public class Repository {
+
+    //TODO: Video lands at 18:05 fpr the repository portion of it.
+
+    private AssessmentDAO mAssessmentDAO;
+    private CourseDAO mCourseDAO;
+    private TermDAO mTermDAO;
+    private List<AssessmentEntity> mAllAssessments;
+    private List<CourseEntity> mAllCourses;
+    private List<TermEntity> mAllTerms;
+
+
+
+
+
 }
