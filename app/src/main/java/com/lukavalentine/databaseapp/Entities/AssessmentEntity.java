8ae8d78af53d7e6afa49be05501e2a2fe7dc4332
Pigeon Tastic <13218687+pigeontastic@users.noreply.github.com>
2021-11-20 12:21:23 -0800

    Finished filling out entities - will handle foreign keys later.

--- app/src/main/java/com/lukavalentine/databaseapp/Entities/AssessmentEntity.java
+++ app/src/main/java/com/lukavalentine/databaseapp/Entities/AssessmentEntity.java
@@ -1,7 +1,81 @@
 package com.lukavalentine.databaseapp.Entities;
 
 import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.PrimaryKey;
 
 @Entity(tableName = "assessment_table")
 public class AssessmentEntity {
+
+    @PrimaryKey(autoGenerate = true)
+    private int assessmentID;
+    private String assessmentName;
+    private String assessmentStart;
+    private String assessmentEnd;
+    private int courseID;
+
+
+
+
+    @Override
+    public String toString() {
+        return "AssessmentEntity{" +
+                "assessmentID=" + assessmentID +
+                ", assessmentName='" + assessmentName + '\'' +
+                ", assessmentStart='" + assessmentStart + '\'' +
+                ", assessmentEnd='" + assessmentEnd + '\'' +
+                ", courseID=" + courseID +
+                '}';
+    }
+
+    public AssessmentEntity(int assessmentID, String assessmentName, String assessmentStart, String assessmentEnd, int courseID) {
+        this.assessmentID = assessmentID;
+        this.assessmentName = assessmentName;
+        this.assessmentStart = assessmentStart;
+        this.assessmentEnd = assessmentEnd;
+        this.courseID = courseID;
+    }
+
+    public int getAssessmentID() {
+        return assessmentID;
+    }
+
+    public void setAssessmentID(int assessmentID) {
+        this.assessmentID = assessmentID;
+    }
+
+    public String getAssessmentName() {
+        return assessmentName;
+    }
+
+    public void setAssessmentName(String assessmentName) {
+        this.assessmentName = assessmentName;
+    }
+
+    public String getAssessmentStart() {
+        return assessmentStart;
+    }
+
+    public void setAssessmentStart(String assessmentStart) {
+        this.assessmentStart = assessmentStart;
+    }
+
+    public String getAssessmentEnd() {
+        return assessmentEnd;
+    }
+
+    public void setAssessmentEnd(String assessmentEnd) {
+        this.assessmentEnd = assessmentEnd;
+    }
+
+    public int getCourseID() {
+        return courseID;
+    }
+
+    public void setCourseID(int courseID) {
+        this.courseID = courseID;
+    }
 }
+
+
+
--- app/src/main/java/com/lukavalentine/databaseapp/Entities/CourseEntity.java
+++ app/src/main/java/com/lukavalentine/databaseapp/Entities/CourseEntity.java
@@ -1,7 +1,94 @@
 package com.lukavalentine.databaseapp.Entities;
 
 import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
 @Entity(tableName = "course_table")
 public class CourseEntity {
+    @PrimaryKey(autoGenerate = true)
+    private int courseID;
+    private String courseName;
+    private String courseInstructor;
+    private String courseNote;
+    private String courseStart;
+    private String courseEnd;
+    private int termID;
+
+    @Override
+    public String toString() {
+        return "CourseEntity{" +
+                "courseID=" + courseID +
+                ", courseName='" + courseName + '\'' +
+                ", courseInstructor='" + courseInstructor + '\'' +
+                ", courseNote='" + courseNote + '\'' +
+                ", courseStart='" + courseStart + '\'' +
+                ", courseEnd='" + courseEnd + '\'' +
+                ", termID=" + termID +
+                '}';
+    }
+
+    public CourseEntity(String courseName, String courseInstructor, String courseNote, String courseStart, String courseEnd, int termID) {
+        this.courseName = courseName;
+        this.courseInstructor = courseInstructor;
+        this.courseNote = courseNote;
+        this.courseStart = courseStart;
+        this.courseEnd = courseEnd;
+        this.termID = termID;
+    }
+
+    public int getCourseID() {
+        return courseID;
+    }
+
+    public void setCourseID(int courseID) {
+        this.courseID = courseID;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseInstructor() {
+        return courseInstructor;
+    }
+
+    public void setCourseInstructor(String courseInstructor) {
+        this.courseInstructor = courseInstructor;
+    }
+
+    public String getCourseNote() {
+        return courseNote;
+    }
+
+    public void setCourseNote(String courseNote) {
+        this.courseNote = courseNote;
+    }
+
+    public String getCourseStart() {
+        return courseStart;
+    }
+
+    public void setCourseStart(String courseStart) {
+        this.courseStart = courseStart;
+    }
+
+    public String getCourseEnd() {
+        return courseEnd;
+    }
+
+    public void setCourseEnd(String courseEnd) {
+        this.courseEnd = courseEnd;
+    }
+
+    public int getTermID() {
+        return termID;
+    }
+
+    public void setTermID(int termID) {
+        this.termID = termID;
+    }
 }
