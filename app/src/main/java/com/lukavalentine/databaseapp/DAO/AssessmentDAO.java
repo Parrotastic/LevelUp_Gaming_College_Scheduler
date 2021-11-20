0ef479b8a70427872d8613c32935fe060292cabf
Pigeon Tastic <13218687+pigeontastic@users.noreply.github.com>
2021-11-20 12:27:50 -0800

    Finished AssessmentDAO.java and CourseDAO.java.

--- app/src/main/java/com/lukavalentine/databaseapp/DAO/AssessmentDAO.java
+++ app/src/main/java/com/lukavalentine/databaseapp/DAO/AssessmentDAO.java
@@ -1,7 +1,29 @@
 package com.lukavalentine.databaseapp.DAO;
 
 
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
 
+import com.lukavalentine.databaseapp.Entities.AssessmentEntity;
+import com.lukavalentine.databaseapp.Entities.TermEntity;
+
+import java.util.List;
 
 public interface AssessmentDAO {
+    @Insert
+    void insert(AssessmentEntity assessment);
+
+    @Update
+    void update(AssessmentEntity assessment);
+
+    @Delete
+    void delete(AssessmentEntity assessment);
+
+    @Query("DELETE FROM assessment_table")
+    void deleteAllAssessments();
+
+    @Query("SELECT * FROM assessment_table ORDER BY assessmentID ASC")
+    List<AssessmentEntity> getAllAssessments();
 }
--- app/src/main/java/com/lukavalentine/databaseapp/DAO/CourseDAO.java
+++ app/src/main/java/com/lukavalentine/databaseapp/DAO/CourseDAO.java
@@ -1,4 +1,28 @@
 package com.lukavalentine.databaseapp.DAO;
 
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.lukavalentine.databaseapp.Entities.AssessmentEntity;
+import com.lukavalentine.databaseapp.Entities.CourseEntity;
+
+import java.util.List;
+
 public interface CourseDAO {
+    @Insert
+    void insert(CourseEntity course);
+
+    @Update
+    void update(CourseEntity course);
+
+    @Delete
+    void delete(CourseEntity course);
+
+    @Query("DELETE FROM course_table")
+    void deleteAllCourses();
+
+    @Query("SELECT * FROM course_table ORDER BY courseID ASC")
+    List<CourseEntity> getAllCourses();
 }
