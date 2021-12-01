29fcdc82875ad5b2c6a1eb078e47e2de2f6f4661
Pigeon Tastic <13218687+pigeontastic@users.noreply.github.com>
2021-11-30 21:01:00 -0800

    Work on CourseAdapter.java started.

--- app/src/main/java/com/lukavalentine/databaseapp/UI/CourseAdapter.java
+++ app/src/main/java/com/lukavalentine/databaseapp/UI/CourseAdapter.java
@@ -1,4 +1,77 @@
 package com.lukavalentine.databaseapp.UI;
 
-public class CourseAdapter {
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.helper.widget.Carousel;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.lukavalentine.databaseapp.Entities.CourseEntity;
+import com.lukavalentine.databaseapp.R;
+
+import java.util.List;
+
+public class CourseAdapter extends RecyclerView.Adapter<CourseAdapter.CourseViewHolder> {
+
+    private final Context context;
+    private final LayoutInflater mInflater;
+    private List<CourseEntity> mCourses;
+
+    public CourseAdapter(Context context){
+        mInflater = LayoutInflater.from(context);
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public CourseViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CourseViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class CourseViewHolder extends RecyclerView.ViewHolder{
+        private final TextView courseTextView;
+        private final TextView courseTextView2;
+
+        private CourseViewHolder(@NonNull View itemView) {
+            super(itemView);
+            courseTextView = itemView.findViewById(R.id.courseTextView);
+            courseTextView2 = itemView.findViewById(R.id.courseTextView2);
+            itemView.setOnClickListener(new View.OnClickListener(){
+
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+                    final CourseEntity current = mCourses.get(position);
+
+                    Intent intent = new Intent(context, CourseDetail.class);
+                    intent.putExtra("courseID", current.getCourseID());
+                    intent.putExtra("courseName", current.getCourseName());
+                    intent.putExtra("courseInstructor", current.getCourseInstructor());
+                    intent.putExtra("courseNote", current.getCourseNote());
+                    intent.putExtra("courseStart", current.getCourseStart());
+                    intent.putExtra("courseEnd", current.getCourseEnd());
+                    intent.putExtra("termID", current.getTermID());
+                    intent.putExtra("position", position);
+                    context.startActivity(intent);
+
+                }
+            });
+
+        }
+    }
 }
